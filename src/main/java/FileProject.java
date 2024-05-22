33112ce1e7372a168f5d6e8a464a8ba6963e3361
shamim <shamimsamangi5@gmail.com>
2024-05-22 23:16:43 +0330

    create the main class

---
+++ src/main/java/FileProject.java
@@ -0,0 +1,10 @@
+import org.hibernate.SessionFactory;
+
+public class FileProject {
+    public static void main(String[] args) {
+        SessionFactory factory = SessionFactorySingleton.getInstance();
+        PersonRepository repository = new PersonRepository();
+        PersonService personService = new PersonService(factory, repository);
+        personService.writeToDB("textFiles/people.txt");
+    }
+}
